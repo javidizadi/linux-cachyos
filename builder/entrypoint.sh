903a328f71dac7ff5142a81fe95e83cd7ef59744
Javid izadi <javid.izadi1384@gmail.com>
2023-07-26 08:29:11 +0330

    [test]

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-set -e
-export PATH="/usr/lib/ccache/bin:$PATH"
-sudo chmod -R a+rw $HOME/.cache/ccache
-cd $HOME
-git clone -b master https://github.com/CachyOS/linux-cachyos
-cd linux-cachyos/$PKG
-echo "Compiling kernel..."
-KBUILD_BUILD_TIMESTAMP='' \
-env _processor_opt="sandybridge" \
-_disable_debug=y \
-_NUMAdisable=y \
-_nr_cpus=4 \
-_use_auto_optimization='' \
-_localmodcfg=y \
-_cc_harder=y \
-makepkg -s --noconfirm
-echo "Logining in to GitHub..."
-printenv GITHUB_KEY | gh auth login --with-token
+# set -e
+# export PATH="/usr/lib/ccache/bin:$PATH"
+# sudo chmod -R a+rw $HOME/.cache/ccache
+# cd $HOME
+# git clone -b master https://github.com/CachyOS/linux-cachyos
+# cd linux-cachyos/$PKG
+# echo "Compiling kernel..."
+# KBUILD_BUILD_TIMESTAMP='' \
+# env _processor_opt="sandybridge" \
+# _disable_debug=y \
+# _NUMAdisable=y \
+# _nr_cpus=4 \
+# _use_auto_optimization='' \
+# _localmodcfg=y \
+# _cc_harder=y \
+# makepkg -s --noconfirm
+# echo "Logining in to GitHub..."
+# printenv GITHUB_KEY | gh auth login --with-token
 minor=$(grep _minor PKGBUILD | head -1 | cut -c 8-)
 major=$(grep _major PKGBUILD | head -1 | cut -c 8-)
 pkgrel=$(grep pkgrel PKGBUILD | head -1 | cut -c 8-)
@@ -26,6 +26,8 @@ if [ -n $scheduler ]; then
 else
     version="v$major.$minor-$pkgrel"
 fi
+echo $scheduler
+exit 0
 repo=$(printenv REPO)
 echo "Checking for same release..."
 set +e
